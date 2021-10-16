304f03d5d6b90381943d268f2d34990c6bc8bf80
michael-2956 <work.michael.2956@gmail.com>
2021-10-16 17:00:26 +0300

    add v2 process_data

---
+++ ClassifierV2.py
@@ -0,0 +1,93 @@
+import pandas as pd
+import string
+import re
+
+class BayesianClassifier:
+    """
+    Implementation of Naive Bayes classification algorithm.
+    """
+    def __init__(self):
+        pass
+
+    def fit(self, X, y):
+        """
+        Fit Naive Bayes parameters according to train data X and y.
+        :param X: pd.DataFrame|list - train input/messages
+        :param y: pd.DataFrame|list - train output/labels
+        :return: None
+        """
+        pass
+
+    def predict_prob(self, message, label):
+        """
+        Calculate the probability that a given label can be assigned to a given message.
+        :param message: str - input message
+        :param label: str - label
+        :return: float - probability P(label|message)
+        """
+        pass
+
+    def predict(self, message):
+        """
+        Predict label for a given message.
+        :param message: str - message
+        :return: str - label that is most likely to be truly assigned to a given message
+        """
+        pass
+
+    def score(self, X, y):
+        """
+        Return the mean accuracy on the given test data and labels - the efficiency of a trained model.
+        :param X: pd.DataFrame|list - test data - messages
+        :param y: pd.DataFrame|list - test labels
+        :return:
+        """
+        pass
+
+
+def process_data(data_file: str) -> tuple:
+    """
+    Function for data processing and split it into X and y sets.
+    :param data_file: str - train data
+    :return: pd.DataFrame|list, pd.DataFrame|list - X and y data frames or lists
+    """
+    df = pd.read_csv(data_file)
+
+    with open("./data/stop_words.txt", mode="r", encoding="ascii") as stop_words_file:
+        stop_words = [word.strip("\n") for word in stop_words_file.readlines()]
+
+    punctuation = string.punctuation.replace("@", "")
+    redundantchars = "â¦"
+    punctuation_trans = str.maketrans(punctuation, " " * len(punctuation), redundantchars)
+    remove_nonascii_re = re.compile(r"(&amp;|[^\x00-\x7F]+)")
+
+    def filter_tweets(tweet: str) -> str:
+        """Filter a tweet from puntuation and stopwords"""
+        print(re.sub(remove_nonascii_re, '', tweet))
+        tweet = " " + re.sub(remove_nonascii_re, '', tweet).lower().translate(punctuation_trans) + " "
+        for word in stop_words:
+            tweet = tweet.replace(" " + word + " ", " ")
+        tweet = tweet.strip(" ")
+        input(tweet)
+        return tweet
+
+    df["tweet"] = df["tweet"].apply(filter_tweets)
+
+    tweets, labels = df["tweet"].values, df["label"].values
+    return tweets, labels
+
+
+if __name__ == "__main__":
+    train_X, train_y = process_data("./data/train.csv")
+    test_X, test_y = process_data("./data/test.csv")
+
+    print(f"{train_X[:10] = }")
+    print(f"{train_y[:10] = }")
+
+    # classifier = BayesianClassifier()
+    # classifier.fit(train_X, train_y)
+    # classifier.predict_prob(test_X[0], test_y[0])
+
+    # print("--"*10)
+    # print(f"model score: {classifier.score(test_X, test_y)}%")
+    # print("--"*10)
